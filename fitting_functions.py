3d5234d6b41777b395d6ad98375630e194db34cb
isaaccasm <isaac.casm@gmail.com>
2020-01-01 11:02:19 +0000

    Allow the use of the input parameters of the fitting models and the initial parameters to be for each moel in order

--- fitting_functions.py
+++ fitting_functions.py
@@ -7,7 +7,7 @@ from statsmodels.stats.diagnostic import acorr_ljungbox
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-def fit_model(data, orders, metric, s=None, min_p=0.01, return_untrained_model=False, show=0):
+def fit_model(data, orders, metric, s=None, min_p=0.01, return_untrained_model=False, show=0, **kwargs):
     """
     Fit a set of models and return the model that minimises a given metric that must be provided by the class ARMAResults
     :param data: The time series to analyse as a 1D numpy array
@@ -20,19 +20,31 @@ def fit_model(data, orders, metric, s=None, min_p=0.01, return_untrained_model=F
                 at least one of the ACF cannot be 0. So, the p value is the probability of the acf to have the value they have
                 and still being actually 0. So, we want a high p-value, since we want the null hypothesis to be True.
     :param return_untrained_model: Not only returned the trained model, but also the model before the fitting it.
-    :param show: int -> 0 do not show anything, 1 show one line with a summary, 2-> Show all the information
+    :param show: int -> 0 do not show anything, 1 show one line with a summary, 2-> Show all the information except exceptions.
+                        3 shows all the information.
+    :param kwargs: Parameters for the fitting function. Check:
+                    https://www.statsmodels.org/stable/generated/statsmodels.tsa.arima_model.ARIMA.fit.html#statsmodels.tsa.arima_model.ARIMA.fit
     :return: The trained ARMAResults model that minimises the given metric.
     """
+    start_params = []
+    if 'disp' not in kwargs:
+        kwargs['disp'] = -1
+    if 'start_params' in kwargs:
+        if hasattr(kwargs['start_params'], '__len__'):
+            if not hasattr(kwargs['start_params'], '__len__'):
+                start_params = kwargs['start_params']
 
     values = []
-    for order in orders:
+    for i, order in enumerate(orders):
         if s is None:
             model = ARIMA(data, order=order)
         else:
             model = SARIMAX(data, order=order[:3], seasonal_order=order[3:]+[s])
 
         try:
-            results_AR = model.fit(disp=-1)
+            if start_params:
+                kwargs['start_params'] = start_params[i]
+            results_AR = model.fit(**kwargs)
 
             residues = results_AR.resid
             results_AR.sse = np.sum(residues**2)
@@ -50,10 +62,12 @@ def fit_model(data, orders, metric, s=None, min_p=0.01, return_untrained_model=F
             if show > 0:
                 print('order:{} - AIC: {:.2f} - BIC: {:.2f} - SSE: {:.2f} - p-value {:.4f}'.format(order, results_AR.aic, results_AR.bic, results_AR.sse, p_value))
 
-        except:# ValueError:
+        except Exception as err:# ValueError:
             #results_AR = model.fit(start_params = [0., 0, 0, 1], disp=-1)
             if show > 0:
                 print('order:{} -- Failed'.format(order))
+            if show > 2:
+                print(repr(err))
             values.append(1e20)
 
     if show > 0:
@@ -81,7 +95,8 @@ def fit_trend_splines(data, nsegs=2):
     index = np.arange(len(data))
     nknots = max(2, nsegs + 1)
     knots = np.linspace(index[0], index[-1], nknots + 2)[1:-2]
-    return LSQUnivariateSpline(index, data, knots)
+    fun = LSQUnivariateSpline(index, data, knots)
+    return fun(index)
 
 def _extrapolate_signals(y1, y2, num_samples=1):
     """
